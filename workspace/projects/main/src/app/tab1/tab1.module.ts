9b9961ec49c33f077327333aae0596cca83ab6a8
shion <tenta_shion@yahoo.co.jp>
2019-09-30 16:58:03 +0900

    main プロジェクトの追加

---
+++ workspace/projects/main/src/app/tab1/tab1.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+import { Tab1Page } from './tab1.page';
+
+@NgModule({
+  declarations: [
+    Tab1Page
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([{ path: '', component: Tab1Page }])
+  ]
+})
+export class Tab1PageModule { }
