91541e3c37c8bb144c1cf92f030da021077c2f44
shion <tenta_shion@yahoo.co.jp>
2019-10-04 01:28:40 +0900

    Capacitor 周りのブラッシュアップ

--- workspace/projects/main/src/app/app.module.ts
+++ workspace/projects/main/src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,8 +18,6 @@ import { AppComponent } from './app.component';
     IonicModule.forRoot()
   ],
   providers: [
-    SplashScreen,
-    StatusBar,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
 
