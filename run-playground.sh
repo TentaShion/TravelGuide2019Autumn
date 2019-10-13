ceeddadbfe8b4f1834ccead2d915107d2a90a696
shion <tenta_shion@yahoo.co.jp>
2019-10-13 12:38:25 +0900

    イメージ名の変更

--- run-playground.sh
+++ run-playground.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -f Playground.Dockerfile -t ionic4angulartemplate .
-docker run --rm -it -p 4200:4200 -p 8100:8100 -p 9876:9876 -p 35729:35729 -p 53703:53703 -v $(pwd)/workspace:/workspace ionic4angulartemplate
+docker build -f Playground.Dockerfile -t travelguide2019autumn .
+docker run --rm -it -p 4200:4200 -p 8100:8100 -p 9876:9876 -p 35729:35729 -p 53703:53703 -v $(pwd)/workspace:/workspace travelguide2019autumn
